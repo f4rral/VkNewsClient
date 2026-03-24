8bf3ad3d1122d6987c54c6cf3a2d5f42e7231b47
f4rral <f4rral@gmail.com>
2026-03-24 17:42:07 +0300

    Log-е ответов от VK API

--- app/src/main/java/com/vknewsclient/data/network/ApiFactory.kt
+++ app/src/main/java/com/vknewsclient/data/network/ApiFactory.kt
@@ -1,5 +1,8 @@
 package com.vknewsclient.data.network
 
+import android.util.Log
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -8,6 +11,25 @@ import retrofit2.create
 
 object ApiFactory {
     private const val BASE_URL = "https://api.vk.ru/method/"
+    private val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .create()
+
+    private val loggingInterceptor = HttpLoggingInterceptor { message ->
+        if (message.startsWith("{") || message.startsWith("[")) {
+            try {
+                val jsonElement = gson.fromJson(message, JsonElement::class.java)
+                val prettyJson = gson.toJson(jsonElement)
+                Log.d("OkHttp", prettyJson)
+            } catch (e: Exception) {
+                Log.d("OkHttp", message)
+            }
+        } else {
+            Log.d("OkHttp", message)
+        }
+    }.apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(HttpLoggingInterceptor().apply {
